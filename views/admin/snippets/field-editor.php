9ec4b169ffad9f8a46ba923f2eafedbc12013cf4
The Yellow Shark <bowdowntozappa@yahoo.com>
2012-02-16 13:26:44 -0700

    Fixed call to wp_editor(), to use proper parameter format
    For more info, see wp/wp-includes/general-template, line ~1806: 
    function wp_editor( $content, $editor_id, $settings = array() )

--- views/admin/snippets/field-editor.php
+++ views/admin/snippets/field-editor.php
@@ -17,6 +17,6 @@
 
     <?php if(!empty($title)):?><h4><?php echo $title; ?></h4><?php endif; ?>
 
-    <?php wp_editor($value, $key, $prev_id = 'title', $media_buttons = true); ?>
+    <?php wp_editor($value, $key,  array('media_buttons' => true)); ?>
 
 </div><!-- .custom_field_type_editor -->
